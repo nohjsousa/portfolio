5115794f3d43eaed16c9f48b69aa4df022d3c7b4
nohjsousa <jlimalion7@gmail.com>
2021-03-12 00:01:51 -0300

    update

---
+++ js/animation.js
@@ -0,0 +1,32 @@
+//Animação do carrossel automático 
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
+
+
+//Fim Animação do carrossel automático
+
+/*! Aos Scroll Down !*/
+
+/*! Fim Scroll Down !*/
+
